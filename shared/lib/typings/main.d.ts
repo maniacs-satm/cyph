15485f48a2cc151048e8fef6f722fde6784eb944
Ryan Lester <hacker@linux.com>
2016-04-11 22:42:56 -0400

    remove redundant type definition (es6-promise)

--- shared/lib/typings/main.d.ts
+++ shared/lib/typings/main.d.ts
@@ -3,7 +3,6 @@
 /// <reference path="main/ambient/angular/index.d.ts" />
 /// <reference path="main/ambient/cryptojs/index.d.ts" />
 /// <reference path="main/ambient/dompurify/index.d.ts" />
-/// <reference path="main/ambient/es6-promise/index.d.ts" />
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/highlightjs/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
