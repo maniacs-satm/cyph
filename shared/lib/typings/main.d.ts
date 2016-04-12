c57394c5de4ce099761773b3dad13a10ce87cced
Ryan Lester <hacker@linux.com>
2016-04-12 01:52:04 +0000

    updatelibs

--- shared/lib/typings/main.d.ts
+++ shared/lib/typings/main.d.ts
@@ -4,6 +4,7 @@
 /// <reference path="main/ambient/cryptojs/index.d.ts" />
 /// <reference path="main/ambient/dompurify/index.d.ts" />
 /// <reference path="main/ambient/es6-promise/index.d.ts" />
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/highlightjs/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
 /// <reference path="main/ambient/webrtc/mediastream/index.d.ts" />
