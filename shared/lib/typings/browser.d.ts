ca50437b9066fb0e527c3e63090504b149da4f6b
Ryan Lester <hacker@linux.com>
2016-04-18 08:55:05 +0000

    remaining issues resolved

--- shared/lib/typings/browser.d.ts
+++ shared/lib/typings/browser.d.ts
@@ -6,5 +6,6 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/highlightjs/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/systemjs/index.d.ts" />
 /// <reference path="browser/ambient/webrtc/mediastream/index.d.ts" />
 /// <reference path="browser/ambient/webrtc/rtcpeerconnection/index.d.ts" />
--- shared/lib/typings/main.d.ts
+++ shared/lib/typings/main.d.ts
@@ -6,5 +6,6 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/highlightjs/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
+/// <reference path="main/ambient/systemjs/index.d.ts" />
 /// <reference path="main/ambient/webrtc/mediastream/index.d.ts" />
 /// <reference path="main/ambient/webrtc/rtcpeerconnection/index.d.ts" />
